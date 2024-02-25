27d25a9f9606fbf10d74261f944acd00ad9c7e93
Prem Raahul K <raahulreddy99@gmail.com>
2024-02-25 09:55:04 +0530

    Exercise- Updating State

--- Projects/react-app/src/App.tsx
+++ Projects/react-app/src/App.tsx
@@ -2,24 +2,34 @@ import { useState } from "react";
 import { produce } from "immer";
 
 function App() {
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["cheese", "jalapenos"],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, name: "Shoes", price: 50, qty: 1 },
+      { id: 2, name: "Shirt", price: 20, qty: 1 },
+    ],
   });
   const handleClick = () => {
-    //setPizza({ ...pizza, toppings: [...pizza.toppings, "pepperoni"] });
-    setPizza(
+    // setCart({
+    //   ...cart,
+    //   items: cart.items.map((item) =>
+    //     item.id === 1 ? { ...item, qty: item.qty + 1 } : item
+    //   ),
+    // });
+    setCart(
       produce((draft) => {
-        draft.toppings.push("pepperoni");
+        draft.items.map((item) => (item.id === 1 ? item.qty++ : item.qty));
       })
     );
   };
   return (
     <div>
-      <h1>{pizza.name}</h1>
+      <h1>Cart</h1>
       <ul>
-        {pizza.toppings.map((topping, index) => (
-          <li key={index}>{topping}</li>
+        {cart.items.map((item) => (
+          <li key={item.id}>
+            {item.name} - ${item.price} - {item.qty}
+          </li>
         ))}
       </ul>
       <button onClick={handleClick}>Click Here</button>

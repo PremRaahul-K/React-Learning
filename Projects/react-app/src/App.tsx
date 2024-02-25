6f52aa1b9a6fc486d15a2a6b835b56667bafa0ea
Prem Raahul K <raahulreddy99@gmail.com>
2024-02-25 09:42:19 +0530

    Exercise- Updating State

--- Projects/react-app/src/App.tsx
+++ Projects/react-app/src/App.tsx
@@ -1,9 +1,28 @@
-import Like from "./components/Like";
+import { useState } from "react";
+import { produce } from "immer";
 
 function App() {
+  const [pizza, setPizza] = useState({
+    name: "Spicy Pepperoni",
+    toppings: ["cheese", "jalapenos"],
+  });
+  const handleClick = () => {
+    //setPizza({ ...pizza, toppings: [...pizza.toppings, "pepperoni"] });
+    setPizza(
+      produce((draft) => {
+        draft.toppings.push("pepperoni");
+      })
+    );
+  };
   return (
     <div>
-      <Like onClick={() => console.log("Clicked")} />
+      <h1>{pizza.name}</h1>
+      <ul>
+        {pizza.toppings.map((topping, index) => (
+          <li key={index}>{topping}</li>
+        ))}
+      </ul>
+      <button onClick={handleClick}>Click Here</button>
     </div>
   );
 }
